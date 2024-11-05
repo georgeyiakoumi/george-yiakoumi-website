72ca11e397845363c7db82836835ac25623af470
George Yiakoumi <George@Georges-MacBook-Pro-2.local>
2024-11-05 01:15:10 +0000

    setup structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/main.scss';
+import Hero from './components/Hero';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Services from './components/Services';
+import Testimonials from './components/Testimonials';
+import Philosophy from './components/Philosophy';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hero />
+      <About />
+      <Portfolio />
+      <Services />
+      <Testimonials />
+      <Philosophy />
+      <Contact />
     </div>
   );
 }
