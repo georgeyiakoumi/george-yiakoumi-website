26867f0d400c47c1b7daa493723676d4b2db71f2
George Yiakoumi <George@Georges-MacBook-Pro-2.local>
2024-11-13 13:49:36 +0000

    added a holding page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import './styles/main.scss';
-import Hero from './components/sections/Hero';
-import About from './components/sections/About';
-import Portfolio from './components/sections/Portfolio';
-import Services from './components/sections/Services';
-import Testimonials from './components/sections/Testimonials';
-import Philosophy from './components/sections/Philosophy';
-import Contact from './components/sections/Contact';
+// App.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Holding from './components/sections/Holding';
 
 function App() {
-  return (
-    <div className="App">
-      <Hero />
-      <About />
-      <Portfolio />
-      <Services />
-      <Testimonials />
-      <Philosophy />
-      <Contact />
-    </div>
-  );
+    return (
+        <div>
+            <Holding />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/data/socialLinks.ts
@@ -0,0 +1,7 @@
+// socialLinks.ts
+export const socialLinks = {
+    linkedin: "https://linkedin.com/in/georgeyiakoumi",
+    twitter: "https://twitter.com/georgeyiakoumi",
+    behance: "https://behance.net/georgeyiakoumi",
+    email: "mailto:gykmi91@protonmail.com"
+};
