428997d5a4a44ae8730aa7a96065bcbbf691e9fd
George Yiakoumi <George@Georges-MacBook-Pro-2.local>
2024-11-13 16:31:31 +0000

    amended social links

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -3,22 +3,22 @@
 export const socialLinks = [
     {
         name: "LinkedIn",
-        url: "https://linkedin.com/in/yourusername",
+        url: "https://linkedin.com/in/georgeyiakoumi",
         ariaLabel: "Visit George Yiakoumi's LinkedIn profile"
     },
     {
         name: "Twitter",
-        url: "https://twitter.com/yourusername",
+        url: "https://twitter.com/georgeyiakoumi",
         ariaLabel: "Visit George Yiakoumi's Twitter profile"
     },
     {
         name: "Behance",
-        url: "https://behance.net/yourusername",
+        url: "https://behance.net/georgeyiakoumi",
         ariaLabel: "Visit George Yiakoumi's Behance profile"
     },
     {
         name: "Email",
-        url: "mailto:yourname@example.com",
+        url: "mailto:gykmi91@protonmail.com",
         ariaLabel: "Send an email to George Yiakoumi"
     }
 ];
